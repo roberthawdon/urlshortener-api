3463cd81c64f2760975fa1e62a07fadde71f8041
Robert Ian Hawdon <git@robertianhawdon.me.uk>
2024-08-04 04:44:59 +0100

    Added more mongo db logging

--- db.js
+++ db.js
@@ -17,12 +17,14 @@ const options = {
 let url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 if (MONGO_REPLICASET) {
-  url += "&replicaSet=${MONGO_REPLICASET}"
+  url += `&replicaSet=${MONGO_REPLICASET}`
 }
+
+console.log('MongoDB Connection URL:', url);
 
 mongoose.connect(url, options).then( function() {
   console.log('MongoDB is connected');
 })
   .catch( function(err) {
-  console.log(err);
+  console.error('MongoDB connection error:', err);
 });
